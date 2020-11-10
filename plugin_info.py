8077b041d6cfaa2616e658c1e75719af8f9ab963
Sébastien Weber <seba.weber@gmail.com>
2020-11-10 14:18:04 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'oceaninsight'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_oceaninsight' #to modify
+description = 'Set of PyMoDAQ plugins for OceanInsight (OceanOptics) spectrometers'
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = []
+##
